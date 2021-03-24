568eed18d36f2dece675b208d68583750a6ddf95
CorpusNoctis <79172142+aela1@users.noreply.github.com>
2021-03-24 18:58:11 +0100

    Añadido proyecto de inicializacion de DB
    A la solución se le ha anexado un proyecto de consola para automatizar la creación e inicialización de la base de datos, las sentencias que se ejecutan de forma automatica se encuentran en los archivos de recursos anexos al proyecto de consola del cual uno es para la creación de la base de datos y el otro la inserción de datos de prueba para realizar las pruebas pertinentes en el sistema cuando sea necesario.

---
+++ PracticaFinal/DatabaseInit/Resources/insertdummydata.sql
@@ -0,0 +1,43 @@
+DELIMITER '//'
+INSERT INTO cochesdaw.usuario
+(id, usuario, passwd, tipoUsuario, nombre, direccion, telefono)
+VALUES
+(NULL, 'JuanitoX', 'secret', 'user', 'Juan Perez', 'dir eccion', '01234'),
+(NULL, 'mariavergara19', 'supersecret', 'user', 'Maria Vergara', 'dir accion', '04321'),
+(NULL, 'xXsuperadminXx', 'cringelord', 'admin', 'Juan Nieves', 'dir rid', '00000')
+//
+INSERT INTO cochesdaw.coche
+(id, nombre, descripcion, marca, modelo, motor, anyo, precio, img)
+VALUES
+(NULL, 'FIAT 500', 'FIAT pequenyo', 'FIAT', '500', 'motor', '2019', 14000, 'route/to/imgage.png'),
+(NULL, 'LAMBO AVENTADOR S', 'Expensive car', 'LAMBORGHINI', 'AVENTADOR', 'V12', '2021', 140000, 'route/to/imgage.png'),
+(NULL, 'FORD MUSTANG 5.0 GT', '4rd', 'FORD', 'MUSTANG', 'V8', '2020', 48000, 'route/to/imgage.png'),
+(NULL, 'Ferrari SF90 SPIDER', 'Hibrido', 'FERRARI', 'SF90SPIDER', 'V8', '2021', 69000, 'route/to/imgage.png'),
+(NULL, 'ALFA ROMEO GIULIA SPRINT PLUS 190 CV', 'Long name is long', 'ALFAROMEO', 'GIULIA', 'V8', '2021', 38000, 'route/to/imgage.png'),
+(NULL, 'TOYOTA YARIS ACTIVE TECH', 'tyt', 'TOYOTA', 'YARIS', 'V8', '2021', 36000, 'route/to/imgage.png')
+//
+INSERT INTO cochesdaw.stock
+(idCoche, unidades)
+VALUES
+(1, 10),
+(2, 10),
+(3, 10),
+(4, 10),
+(5, 10),
+(6, 10)
+//
+INSERT INTO cochesdaw.pedido
+(id, idCliente, precioTotal)
+VALUES
+(NULL, 1, 84000),
+(NULL, 1, 69000),
+(NULL, 2, 140000)
+//
+INSERT INTO cochesdaw.lineapedido
+(idPedido, idCoche, precioCoche)
+VALUES
+(1, 3, 48000),
+(1, 6, 36000),
+(2, 4, 69000),
+(3, 2, 140000)
+//
