b3c4648d49a2492f94e18fde4a9cfb3642af093f
aela1 <aela1@gcloud.ua.es>
2021-04-02 18:51:26 +0200

    Preparing the api

--- PracticaFinal/PracticaFinal/Global.asax.cs
+++ PracticaFinal/PracticaFinal/Global.asax.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace PracticaFinal
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
+        protected void Application_Start(object sender, EventArgs e)
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
